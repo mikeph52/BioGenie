4d63e27f528331435702adf7788344783919c0b3
mikeph52 <mikeph526@outlook.com>
2026-02-22 22:33:30 +0200

    BioGenie v.0.31.0 init

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // BioGenie by mikeph_ 2025-2026
-// Current version 0.30.2 21/2/2026
+// Current version 0.31.0 22/2/2026
 #include <iostream>
 #include <string>
 #include <cctype>
@@ -19,7 +19,7 @@
 // Public Functions 
 void title(){
     std::cout << "-----------------------\n";
-    std::cout << "BioGenie 0.30.2 \nby mikeph_ 2025-2026\n\n";    
+    std::cout << "BioGenie 0.31.0 \nby mikeph_ 2025-2026\n\n";    
 }
 void helpme(){
     std::cout << "-----------------------DOCUMENTATION-----------------------\n";
